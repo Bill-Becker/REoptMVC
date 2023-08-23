2346b486f8c1c0022d34c9277ec19974545511d2
Bill Becker <William.Becker@nrel.gov>
2023-08-23 11:19:34 -0600

    Add REST API start

--- app/resources/runs/RunsController.jl
+++ app/resources/runs/RunsController.jl
@@ -4,8 +4,26 @@ using Genie.Renderer, Genie.Renderer.Html, Genie.Router, Genie.Requests
 using SearchLight
 using JSON
 
+function runs()
+    #data = getpayload()  # For convert all query params to Dict{Symbol, Any} for GET requests, from Genie.Requests module
+    #data = postpayload()  # For getting POST payload in Dict{Symbol, Any}, from Genie.Requests module
+    runs = if params(:filter, "") == "optimal" # For getting query param directly; :filter is not a Run field, just a [standard] query
+        find(Run, status = "optimal")
+    elseif params(:filter, "") == "error"
+        find(Run, status = "error")
+    else
+        # First step at pagination but only implemented for API, not UI implemented
+        # this line was: all(Run) and is changed to:
+        all(
+            Run; 
+            limit = params(:limit, SearchLight.SQLLimit_ALL) |> SQLLimit,
+            offset = (parse(Int, params(:page, "1"))-1) * parse(Int, params(:limit, "0"))
+        )
+    end
+end
+
 function index()
-    html(:runs, :index; runs = all(Run))
+    html(:runs, :index; runs = runs())  #add args/vars to end, like runs
 end
 
 function create()  
@@ -16,10 +34,80 @@ function create()
     end
     run_inst = Run(input_dict)
     if save(run_inst)
-      redirect("/?success=Valid .json file $(filespayload("input").name) successfully uploaded")
+        redirect("/?success=Valid .json file $(filespayload("input").name) successfully uploaded")
     else
-      redirect("/?error=Could not save input&input=$(filespayload("input").name)")
+        redirect("/?error=Could not save input&input=$(filespayload("input").name)")
     end
 end
 
+### API
+module API
+module V1
+using REoptMVC.Runs
+using Genie.Router
+using Genie.Renderers.Json
+using ....RunsController
+using Genie.Requests
+using SearchLight.Validation
+using SearchLight
+
+# Simple function to demonstrate that RunsController.[function] is used for both UI and API (below) for DRY
+function list()
+  RunsController.runs() |> json
 end
+
+function item()
+end
+
+function check_payload(payload = Requests.jsonpayload())
+    isnothing(payload) && throw(
+        JSONException(status = BAD_REQUEST, message = "Invalid JSON message received"),
+    )
+    payload
+end
+
+function persist(run)
+    validator = validate(run)
+    if haserrors(validator)
+        return JSONException(status = BAD_REQUEST, message = errors_to_string(validator)) |>
+               json
+    end
+
+    try
+        if ispersisted(run)
+            save!(run)
+            json(run, status = OK)
+        else
+            save!(run)
+            json(
+                run,
+                status = CREATED,
+                headers = Dict("Location" => "/api/v1/runs/$(run.id)"),
+            )
+        end
+    catch ex
+        JSONException(status = INTERNAL_ERROR, message = string(ex)) |> json
+    end
+end
+
+function create()
+    payload = try
+        check_payload()
+    catch ex
+        return json(ex)
+    end
+    run = Run(payload)
+    persist(run)
+end
+
+function update()
+end
+
+function delete()
+end
+
+end # V1
+
+end # API
+
+end # RunsController Module
--- routes.jl
+++ routes.jl
@@ -9,3 +9,9 @@ using REoptMVC.RunsController
 route("/", RunsController.index)
 
 route("/runs", RunsController.create, method = POST)
+
+route("/api/v1/runs", RunsController.API.V1.list, method = GET)
+route("/api/v1/runs/:id::Int", RunsController.API.V1.item, method = GET)
+route("/api/v1/runs", RunsController.API.V1.create, method = POST)
+route("/api/v1/runs/:id::Int", RunsController.API.V1.update, method = PATCH)
+route("/api/v1/runs/:id::Int", RunsController.API.V1.delete, method = DELETE)
